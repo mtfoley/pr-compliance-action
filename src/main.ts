1b2da572b9b0031fe0508445bf9caf201549d908
mtfoley <mtfoley.mae@gmail.com>
2021-09-29 11:46:28 -0400

    feat: implement files to watch

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import {checkBody, checkTitle, checkBranch} from './checks'
 const repoToken = core.getInput('repo-token', {required: true})
 const bodyRegexInput = core.getInput('body-regex')
 const protectedBranch = core.getInput('protected-branch')
+const filesToWatch = core.getMultilineInput('watch-files')
 const client = github.getOctokit(repoToken)
 async function run(): Promise<void> {
   try {
@@ -13,10 +14,15 @@ async function run(): Promise<void> {
     const body = ctx.payload.pull_request?.body ?? ''
     const title = ctx.payload.pull_request?.title ?? ''
     const branch = ctx.payload.pull_request?.head?.ref ?? ''
+    const filesModified = await listFiles({...pr, pull_number: pr.number})
     const bodyCheck = checkBody(body, bodyRegexInput)
     const titleCheck = await checkTitle(title)
     const branchCheck = checkBranch(branch, protectedBranch)
-    const prCompliant = bodyCheck && titleCheck && branchCheck
+    const filesFlagged = filesModified
+      .map(file => file.filename)
+      .filter(filename => filesToWatch.includes(filename))
+    const prCompliant =
+      bodyCheck && titleCheck && branchCheck && filesFlagged.length == 0
     if (!prCompliant) {
       if (!bodyCheck)
         core.warning(`This PR description does not refer to an issue`)
@@ -26,10 +32,21 @@ async function run(): Promise<void> {
         core.error(
           `This PR's title should conform to conventional commit messages`
         )
+      if (filesFlagged.length > 0)
+        core.warning(
+          `This PR modifies the following files: ${filesFlagged.join(', ')}`
+        )
     }
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
 }
-
+async function listFiles(pullRequest: {
+  owner: string
+  repo: string
+  pull_number: number
+}) {
+  const {data: files} = await client.rest.pulls.listFiles(pullRequest)
+  return files
+}
 run()
