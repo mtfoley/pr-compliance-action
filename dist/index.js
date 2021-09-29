3024ddf9320fb5b3afc4f20f287ce11a78f161f0
mtfoley <mtfoley.mae@gmail.com>
2021-09-29 11:16:35 -0400

    chore: package

--- dist/index.js
+++ dist/index.js
@@ -125,14 +125,12 @@ function run() {
             const branchCheck = (0, checks_1.checkBranch)(branch, protectedBranch);
             const prCompliant = bodyCheck && titleCheck && branchCheck;
             if (!prCompliant) {
-                const failMessage = `${bodyCheck ? '' : 'PR must refer to an issue.'}
-        ${titleCheck
-                    ? ''
-                    : 'PR Title must conform to @commitlint/conventional-commit'}
-        ${branchCheck
-                    ? ''
-                    : 'PR must not have "' + protectedBranch + '" as it\'s head branch'}`;
-                core.setFailed(failMessage);
+                if (!bodyCheck)
+                    core.warning(`This PR description does not refer to an issue`);
+                if (!branchCheck)
+                    core.error(`This PR has ${protectedBranch} as its head branch`);
+                if (!titleCheck)
+                    core.error(`This PR's title should conform to conventional commit messages`);
             }
         }
         catch (error) {
