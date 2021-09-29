f0d3d9ebf450c89cf11f6aaa5075cab3263ea126
mtfoley <mtfoley.mae@gmail.com>
2021-09-29 10:10:06 -0400

    fix: correct eslint for file naming

--- src/checks.ts
+++ src/checks.ts
@@ -7,19 +7,19 @@ Checks:
 - Check whether certain files change
 
 */
-import lint from '@commitlint/lint';
-import * as conventionalOpts from '@commitlint/config-conventional';
+import lint from '@commitlint/lint'
+import * as conventionalOpts from '@commitlint/config-conventional'
 
-function checkBody(body:string, regexString: string): Boolean {
-    const regex = new RegExp(regexString,"mi");
-    return regex.test(body);
+function checkBody(body: string, regexString: string): Boolean {
+  const regex = new RegExp(regexString, 'mi')
+  return regex.test(body)
 }
 
 function checkBranch(branch: string, protectedBranch: string): Boolean {
-    return branch !== protectedBranch;
+  return branch !== protectedBranch
 }
 async function checkTitle(title: string): Promise<Boolean> {
-    const {valid} = await lint(title,conventionalOpts.rules,conventionalOpts);
-    return valid;
+  const {valid} = await lint(title, conventionalOpts.rules, conventionalOpts)
+  return valid
 }
-export { checkBody, checkBranch, checkTitle };
+export {checkBody, checkBranch, checkTitle}
