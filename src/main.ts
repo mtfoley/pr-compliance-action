aa83b1101dea72d82a5897afe0a06b0909d04228
Matthew <mtfoley.mae@gmail.com>
2021-11-17 23:42:39 -0500

    use context.repository.default_branch

--- src/main.ts
+++ src/main.ts
@@ -26,7 +26,7 @@ async function run(): Promise<void> {
     const pr = ctx.issue
     const isDraft = (ctx.payload.pull_request?.draft ?? false) === true
     const repoOwner = context.repo.owner
-    if(protectedBranch === '') protectedBranch = context.repo.default_branch;
+    if(protectedBranch === '') protectedBranch = ctx.payload.repository.default_branch;
     const isClosed =
       (ctx.payload.pull_request?.state ?? 'open').toLowerCase() === 'closed'
     if (isClosed) {
